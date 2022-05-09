71228ff971100021053413bc647e8c3a42c48c18
James Leslie <jamesleslie@posteo.net>
2022-05-10 00:00:19 +0100

    [nix] Improve derivation and packaging style
    Each font is now specified by its own derivation. The defaultPackage
    is the "union" of the other font derivations, giving one derivation
    with all of the specified fonts.

--- flake.nix
+++ flake.nix
@@ -1,32 +1,35 @@
 {
-  description = "A flake giving access to fonts that I use, outside of nixpkgs.";
+  description =
+    "A flake giving access to fonts that I use, outside of nixpkgs.";
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        # Fetches the Gill Sans font
-        gillsans = pkgs.fetchzip { url = "https://cdn.freefontsvault.com/wp-content/uploads/2020/02/03141445/Gill-Sans-Font-Family.zip";
-                                   sha256 = "sha256-YcZUKzRskiqmEqVcbK/XL6ypsNMbY49qJYFG3yZVF78=";
-                                   stripRoot = false; };
-      in
-        {
-          defaultPackage = pkgs.stdenv.mkDerivation {
-            pname = "myfonts";
-            version = "0.1.2";
-            dontConfigure = true;
-            src = ./.;
-            src1 = gillsans;
-            installPhase = ''
-                           mkdir -p $out/share/fonts
-                           cp -R $src1 $out/share/fonts/opentype/
-                           '';
-            meta = { description = "Gill Sans font"; };
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        defaultPackage = pkgs.symlinkJoin {
+          name = "myfonts";
+          version = "0.1.3";
+          paths = with self.packages.${system}; [ gillsans ]; # Add font derivation names here
+        };
+
+        packages.gillsans = pkgs.stdenvNoCC.mkDerivation {
+          name = "gillsans-font";
+          dontConfigue = true;
+          src = pkgs.fetchzip {
+            url =
+              "https://cdn.freefontsvault.com/wp-content/uploads/2020/02/03141445/Gill-Sans-Font-Family.zip";
+            sha256 = "sha256-YcZUKzRskiqmEqVcbK/XL6ypsNMbY49qJYFG3yZVF78=";
+            stripRoot = false;
           };
-        }
-    );
+          installPhase = ''
+            mkdir -p $out/share/fonts
+            cp -R $src $out/share/fonts/opentype/
+          '';
+          meta = { description = "A Gill Sans Font Family derivation."; };
+
+        };
+      });
 }
